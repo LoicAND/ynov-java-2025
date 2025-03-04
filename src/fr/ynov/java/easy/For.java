350cc5dca647e4cf3950888428c3942058bc524d
Andrianarivony <loic.andrianarivony@ynov.com>
2025-03-04 16:34:37 +0100

    Exercise Loops

---
+++ src/fr/ynov/java/easy/For.java
@@ -0,0 +1,10 @@
+package fr.ynov.java.easy;
+
+public class For {
+    public static void main(String[] arg) {
+        for (int i= 1; i<11 ; i++){
+            System.out.println(i);
+        }
+    }
+
+}
